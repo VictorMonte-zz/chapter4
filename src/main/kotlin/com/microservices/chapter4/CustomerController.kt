8ea4b0a27bb899b23d0c1573d6d188dc1fc39ad9
Victor Monte <victorbcm@hotmail.com>
2018-07-15 16:32:58 -0300

    Add customer controller

---
+++ src/main/kotlin/com/microservices/chapter4/CustomerController.kt
@@ -0,0 +1,15 @@
+package com.microservices.chapter4
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+    @GetMapping(value = "/customer/{id}")
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer> {
+        return ResponseEntity(Customer(id, "customer $id"), HttpStatus.OK)
+    }
+}
